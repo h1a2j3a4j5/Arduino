7d1488ba88e941f7ccfe77a78aea0ccd33caad87
YoniHagag <73844661+h1a2j3a4j5@users.noreply.github.com>
2022-07-08 14:22:07 +0300

    Add files via upload

---
+++ Lock project/sketch_mar07a/webPage.ino
@@ -0,0 +1,75 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h> 
+
+IPAddress   apIP(55, 55, 55, 40); 
+  const char* ssid = "YONI";
+  ESP8266WebServer server(80); 
+
+
+void wifi_setup () {
+      WiFi.mode(WIFI_AP_STA);
+      WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+      WiFi.softAP(ssid);
+      server.on("/", handleRoot); 
+      server.onNotFound(handleNotFound);
+      server.begin(); 
+}
+
+
+void handleRoot() {
+   String number;
+if(server.hasArg("one")) {
+   number = server.arg("send"); 
+   int allCode = number.toInt();
+
+   for(int x = number.length()-1; x>=0 ;x--){
+       correctCode[x]= allCode % 10;
+       allCode /= 10;       
+   }
+
+}
+
+  
+char html[2000]="";
+strcat(html,"    <html>");
+strcat(html,"    <!DOCTYPE html>\n");
+strcat(html,"    <html lang=\"en\">\n");
+strcat(html,"    <head>\n");
+strcat(html,"    <meta charset=\"UTF-8\">\n");
+strcat(html,"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
+strcat(html,"    <title>Document</title>\n");
+strcat(html,"    <script src=\"./script.js\"></script>\n");
+strcat(html,"    </head>\n");
+strcat(html,"    <body>\n");
+strcat(html,"    <form action=\"\" method=\"get\">\n");
+strcat(html,"    <P>\n");
+strcat(html,"    <input id=\"send\" type=\"text\" value=\"\" name=\"send\">\n");
+strcat(html,"    </P>\n");
+strcat(html,"    <button type=\"submit\" name=\"one\">press</button>\n");
+strcat(html,"    </form>\n");
+strcat(html,"    </body>\n");
+strcat(html,"    </html>");
+server.send(200, "text/html", html);
+}
+
+
+
+void handleNotFound ()  {
+      String message = "File Not Foundnn";
+      message += "URI: ";
+      message += server.uri();
+      message += "nMethod: ";
+      message += (server.method() == HTTP_GET) ? "GET" : "POST";
+      message += "nArguments: ";
+      message += server.args();
+      message += "n";
+      for (uint8_t i = 0; i < server.args(); i++) {
+          message += " " + server.argName(i) + ": " + server.arg(i) + "n";
+      }
+      server.send(404, "text/plain", message);
+}
+
+void wifi_loop() {
+  server.handleClient();
+  delay(10);
+}
